afaca3658a271170ff6d36263df6f682ccecfdc9
caterhamcomputing <caterhamcomputing@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-10 08:34:50 +0000

    * Added the `words` parameter. When set to a value greater than 0, the number of words in the excerpt will be trimmed if greater than the specified value.
    git-svn-id: https://plugins.svn.wordpress.org/cc-child-pages/tags/1.6@1022809 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: CC Child Pages
+Plugin URI: http://ccchildpages.ccplugins.co.uk/
+Description: Show links to child pages
+Author: Tim lomas
+Text Domain: cc-child-pages
+Domain Path: /languages
+Version: 1.6
+Author URI: http://www.caterhamcomputing.net/
+*/
+include_once('includes/ccchildpages.php');
+
+add_shortcode( 'child_pages', 'ccchildpages::show_child_pages' );
+add_action( 'wp_enqueue_scripts', 'ccchildpages::enqueue_styles' );
+add_action( 'plugins_loaded', 'ccchildpages::load_plugin_textdomain' );
+
+/*EOF*/
