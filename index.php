0594ddfb9e6a5414299b9fa8cf28d15f7ca87b88
caterhamcomputing <caterhamcomputing@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-17 17:33:30 +0000

    * CC Child Pages widget enhanced to allow display of children of current page or a specific page * CC Child Pages widget enhanced to allow depth to be specified
    git-svn-id: https://plugins.svn.wordpress.org/cc-child-pages/tags/1.8@1027399 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: CC Child Pages
+Plugin URI: http://ccchildpages.ccplugins.co.uk/
+Description: Show links to child pages
+Author: Caterham Computing
+Text Domain: cc-child-pages
+Domain Path: /languages
+Version: 1.8
+Author URI: http://www.caterhamcomputing.net/
+*/
+include_once('includes/ccchildpages.php');
+
+add_shortcode( 'child_pages', 'ccchildpages::show_child_pages' );
+add_action( 'wp_enqueue_scripts', 'ccchildpages::enqueue_styles' );
+add_action( 'plugins_loaded', 'ccchildpages::load_plugin_textdomain' );
+
+include_once('includes/ccchildpages_widget.php');
+// register widget
+function register_ccchildpages_widget() {
+    register_widget( 'ccchildpages_widget' );
+}
+add_action( 'widgets_init', 'register_ccchildpages_widget' );
+
+/*EOF*/
